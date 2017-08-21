ced01936c2fd16129bf0d93c9c36799a3baf8c75
sgbcoder <ghanesh.balaji1995@gmail.com>
2017-08-21 21:44:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function createTemplate(data){
     var title= data.title;
     var heading = data.heading;
     var date= data.date;
-    var content=data.content;
+    var body=data.content;
 var htmltemplate = `<html>
     <head>
         <title>

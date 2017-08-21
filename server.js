67ebcdce89c195f784481387e38c297b5ebfc222
sgbcoder <ghanesh.balaji1995@gmail.com>
2017-08-21 22:51:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,25 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles{
-    article-one : {
-    title:'Article One | Shreeghanesh',
-    heading:'Article One',
-    date:'21 August 2017',
-    content:`       <p>
-                    This is the content of my Article This is the content of my Article This is the content of my Article
-                    This is the content of my Article This is the content of my Article This is the content of my Article
-                    </p>
-                    <p>
-                    This is the content of my Article This is the content of my Article This is the content of my Article
-                    This is the content of my Article This is the content of my Article This is the content of my Article
-                    </p>
-                    <p>
-                    This is the content of my Article This is the content of my Article This is the content of my Article
-                    This is the content of my Article This is the content of my Article This is the content of my Article
-                    </p>`
-},
-    article-two : {
+
+var articles = {
+     'article-one' : {
     title:'Article Two | Shreeghanesh',
     heading:'Article One',
     date:'22 August 2017',
@@ -40,7 +24,24 @@ var articles{
                     This is the content of my Article This is the content of my Article This is the content of my Article
                     </p>`
 },
-    article-three : {
+     'article-two' : {
+    title:'Article Two | Shreeghanesh',
+    heading:'Article One',
+    date:'22 August 2017',
+    content:`       <p>
+                    This is the content of my Article This is the content of my Article This is the content of my Article
+                    This is the content of my Article This is the content of my Article This is the content of my Article
+                    </p>
+                    <p>
+                    This is the content of my Article This is the content of my Article This is the content of my Article
+                    This is the content of my Article This is the content of my Article This is the content of my Article
+                    </p>
+                    <p>
+                    This is the content of my Article This is the content of my Article This is the content of my Article
+                    This is the content of my Article This is the content of my Article This is the content of my Article
+                    </p>`
+},
+     'article-three' : {
     title:'Article One | Shreeghanesh',
     heading:'Article Three',
     date:'21 August 2017',
@@ -56,44 +57,46 @@ var articles{
                     This is the content of my Article This is the content of my Article This is the content of my Article
                     This is the content of my Article This is the content of my Article This is the content of my Article
                     </p>`
-}
+
 };
+}
 function createTemplate(data){
     var title= data.title;
     var heading = data.heading;
     var date= data.date;
-    var body=data.content;
-    var htmltemplate = `<html>
-    <head>
-        <title>
-            ARTICLE ONE | SHREEGHANESH
-        </title>
-        
-       <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-            
-    <body>
-        <div class="container">
-            <div>
-                <a href="/">Home</a>
-            </div>
-            <hr/>
-            <h3>
-                ${heading}
-            </h3>
-            <div>
-                ${date}
-            </div>
-                <div>
-                    <p>
-                        ${body}
-                    </p>
-                </div>
-        </div>    
-    </body>
-    </html>
-    `;
-return htmltemplate;}
+    var content=data.content;
+            var htmltemplate = `<html>
+                                    <head>
+                                            <title>
+                                                ${title}
+                                            </title>
+                                       <link href="/ui/style.css" rel="stylesheet" />
+                                    </head>
+                                                
+                                        <body>
+                                            <div class="container">
+                                                <div>
+                                                    <a href="/">Home</a>
+                                                </div>
+                                                <hr/>
+                                                <h3>
+                                                    ${heading}
+                                                </h3>
+                                                <div>
+                                                    ${date}
+                                                </div>
+                                                    <div>
+                                                        <p>
+                                                            ${content}
+                                                        </p>
+                                                    </div>
+                                            </div>    
+                                        </body>
+                                </html>
+                                `;
+            return htmltemplate;
+    
+}
 
     app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;//to extract article name this is provided by the express framework

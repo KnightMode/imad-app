137b2ff770a0b18135c354922f862baa873fc01d
sgbcoder <ghanesh.balaji1995@gmail.com>
2017-08-22 00:11:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,11 @@ function createTemplate(data){
     
 }
 
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
     app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;//to extract article name this is provided by the express framework
     res.send(createTemplate(articles[articleName]));
@@ -90,11 +95,6 @@ app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString());
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

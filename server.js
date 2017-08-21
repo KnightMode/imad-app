53c3698c3cd529dbcbd321b89b9df94dd6a80dfa
sgbcoder <ghanesh.balaji1995@gmail.com>
2017-08-22 00:09:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,15 +86,15 @@ function createTemplate(data){
     res.send(createTemplate(articles[articleName]));
     });
 
-    app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-// var counter=0;
-// app.get('/counter', function (req, res) {
-//     counter=counter+1;
-//   res.send(counter.toString());
-// });
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

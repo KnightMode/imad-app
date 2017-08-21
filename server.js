6325c8d6dfc2e4e2fd85ea94ed3aaac49d04cbf0
sgbcoder <ghanesh.balaji1995@gmail.com>
2017-08-21 17:34:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/articleone',function (req,res){
-    res.send('You have requested for Article one');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }
 );
 

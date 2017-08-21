8b1edefd62d710f270a66fc1a87fb9588100c91d
sgbcoder <ghanesh.balaji1995@gmail.com>
2017-08-21 23:34:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,13 @@ console.log('Loaded!');
 //Move image
 
 var img = document.getElementById('pic');
+var moveright=0;
+function moveleft()
+{
+    moveright=moveright+10;
+    img.style.marginright= marginRight+'px';
+}
+var interval = setInterval(moveleft,100);
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    img.style.marginRight = '100px';
 };
